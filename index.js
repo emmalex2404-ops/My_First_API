6e805cd7254fceb93e774e643afe0b1b57588883
Emmanuel Hernandez <emmalex2404@gmail.com>
2026-06-07 19:24:06 -0600

    Agregar frontend y configurar archivos estáticos

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+
   const express = require('express');
   const pool = require('./db');
   const app = express();
   // Para que Express entienda JSON
   app.use(express.json()); 
+  app.use(express.static('public'));
 
 
   // Hola mundo
